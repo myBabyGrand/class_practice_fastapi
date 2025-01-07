39ec7780a1e51a6071734660dfd221e031c95da5
mybabygrand <amuro_ray@kakao.com>
2025-01-07 22:29:31 +0900

    get method done.

--- todos/src/main.py
+++ todos/src/main.py
@@ -7,5 +7,34 @@ def health_check_handler():
     return {"ping": "pong"}
 
 todo_data = {
-
+    1:{
+        "id": 1,
+        "contents": "실전! FastAPI 섹션 1 수강",
+        "is_done": True,
+    },
+    2: {
+        "id": 2,
+        "contents": "실전! FastAPI 섹션 2 수강",
+        "is_done": False,
+    },
+    3: {
+        "id": 3,
+        "contents": "실전! FastAPI 섹션 3 수강",
+        "is_done": False,
+    }
 }
+
+# @app.get("/todos")
+# def get_todos_handler():
+#     return list(todo_data.values())
+
+@app.get("/todos")
+def get_todos_handler2(order: str | None = None):
+    ret = list(todo_data.values())
+    if order and order == "DESC":
+        return ret[::-1]
+    return ret
+
+@app.get("/todos/{todo_id}")
+def get_todo_handler(todo_id : int):
+    return todo_data.get(todo_id, {})
