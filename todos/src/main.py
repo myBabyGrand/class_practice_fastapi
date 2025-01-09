b65672b19b189d4ddbe3f7f6c917ee8a108c8183
mybabygrand <amuro_ray@kakao.com>
2025-01-09 23:37:08 +0900

    api modification : get list

--- todos/src/main.py
+++ todos/src/main.py
@@ -1,5 +1,12 @@
-from fastapi import FastAPI, Body, HTTPException
+from database.connection import get_db
+from fastapi import FastAPI, Body, HTTPException, Depends
 from pydantic import BaseModel
+from schema.response import ToDoSchema, ListToDoResponse
+from sqlalchemy.orm import Session
+from database.repository import get_todos
+from database.orm import ToDo
+from typing import List
+
 
 app = FastAPI()
 
@@ -29,12 +36,66 @@ todo_data = {
 # def get_todos_handler():
 #     return list(todo_data.values())
 
+# @app.get("/todos", status_code=200)
+# def get_todos_handler2(order: str | None = None):
+#     ret = list(todo_data.values())
+#     if order and order == "DESC":
+#         return ret[::-1]
+#     return ret
+#
+# @app.get("/todos/{todo_id}", status_code=200)
+# def get_todo_handler(todo_id : int):
+#     todo= todo_data.get(todo_id)
+#     if todo:
+#         return todo
+#     raise HTTPException(status_code=404, detail="ToDo Not Found")
+#
+#
+# class CreateTodoRequest(BaseModel):
+#     id: int
+#     contents: str
+#     is_done: bool
+#
+# @app.post("/todos", status_code=201)
+# def create_todo_handler(request: CreateTodoRequest):
+#     todo_data[request.id] = request.dict()
+#     return todo_data[request.id]
+#
+#
+# @app.patch("/todos/{todo_id}", status_code=200)
+# def update_todo_handler(
+#         todo_id: int,
+#         is_done: bool = Body(..., embed=True)
+# ):
+#     todo = todo_data.get(todo_id)
+#     if todo:
+#         todo["is_done"] = is_done
+#         return todo
+#     raise HTTPException(status_code=404, detail="ToDo Not Found")
+#
+# @app.delete("/todos/{todo_id}", status_code=204)
+# def delete_todo_handler(todo_id: int):
+#     todo = todo_data.pop(todo_id, None)
+#     if todo:
+#         return
+#     raise HTTPException(status_code=404, detail="ToDo Not Found")
+
 @app.get("/todos", status_code=200)
-def get_todos_handler2(order: str | None = None):
-    ret = list(todo_data.values())
+def get_todos_handler2(
+    order: str | None = None,
+    session: Session = Depends(get_db)
+) -> ListToDoResponse:
+    todos: List[ToDo] = get_todos(session=session)
+
     if order and order == "DESC":
-        return ret[::-1]
-    return ret
+        return ListToDoResponse(
+            todos=[ToDoSchema.from_orm(todo) for todo in todos[::-1]]
+        )
+    return ListToDoResponse(
+        todos=[ToDoSchema.from_orm(todo) for todo in todos]
+    )
+
+
 
 @app.get("/todos/{todo_id}", status_code=200)
 def get_todo_handler(todo_id : int):
@@ -72,3 +133,4 @@ def delete_todo_handler(todo_id: int):
     if todo:
         return
     raise HTTPException(status_code=404, detail="ToDo Not Found")
+
